80a7b33144656e324d048a02050cba42c6aed1b0
Dinh Phu Nguyen <dinhphhu.nguyen.20@gmail.com>
2024-01-15 11:14:46 +0700

    Add nvim config at office FPT

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.lang.java" },
+    -- { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
--- nvim/lua/plugins/nvim_cmp.lua
+++ nvim/lua/plugins/nvim_cmp.lua
@@ -1,3 +1,7 @@
+-- if true then
+--   return {}
+-- end
+
 return {
   {
     "hrsh7th/nvim-cmp",
