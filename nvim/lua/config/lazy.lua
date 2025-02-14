5cc5e95b5662d469370c9479c85e32acceec5802
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2025-02-14 12:43:18 +0700

    Add LazyVim Extras Clojure
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.clojure" },
     { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
     -- import/override with your plugins
     { import = "plugins" },
